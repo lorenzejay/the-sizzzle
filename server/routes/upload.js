9d91f243e062e92dcea683129d9e053e350c717e
Lorenze Jay <Lorenzejay@gmail.com>
2021-01-31 09:49:31 -0800

    upload route -upload action, retrieve the one that was just posted, retrieve all users, and retrieve user post

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,14 +1,99 @@
 const router = require("express").Router();
 const pool = require("../db.js");
 const authorization = require("../middleware/authorization");
+const { cloudinary } = require("../utils/cloudinary");
 
-router.post("/submit", async (req, res) => {
+//submit post
+router.post("/", authorization, async (req, res) => {
   try {
-    const fileStr = req.body;
-    console.log(fileStr);
+    const fileStr = req.body.data; //image uploaded
+    const { title, description } = req.body;
+    const user_id = req.user;
+
+    const uploadImageResponse = await cloudinary.uploader.upload(fileStr, {
+      use_filename: true,
+    });
+    console.log(uploadImageResponse);
+    const query = await pool.query(
+      "INSERT INTO uploads (uploaded_by, title, description, cloudinary_id, image_url) VALUES ($1, $2 ,$3, $4, $5) RETURNING *",
+      [user_id, title, description, uploadImageResponse.public_id, uploadImageResponse.secure_url]
+    );
+
+    const result = query.rows[0];
+
+    res.status(201).json({
+      data: {
+        message: "Image Uploaded Successfully",
+        user_id: result.user_id,
+        title: result.title,
+        description: result.description,
+        cloudinary_id: result.cloudinary_id,
+        image_url: result.image_url,
+      },
+    });
+    // console.log("success");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ err: "Something went wrong" });
+  }
+});
+
+//GET THE image we just posted
+router.get("/retrieve-image/:cloudinary_id", authorization, async (req, res) => {
+  try {
+    //get the cloudinary_id
+    const { cloudinary_id } = req.params;
+    const user_id = req.user;
+    console.log(user_id);
+    //query based on the id
+    const query = await pool.query(
+      "SELECT * FROM uploads WHERE cloudinary_id = $1 AND uploaded_by = $2",
+      [cloudinary_id, user_id]
+    );
+    const result = query.rows[0];
+
+    res.status(200).json({ result });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+//get all users posts based on their user id
+router.get("/get-user-post/:user_id", async (req, res) => {
+  try {
+    const { user_id } = req.params;
+    const query = await pool.query("SELECT * FROM uploads WHERE uploaded_by = $1", [user_id]);
+    const result = query.rows;
+
+    res.status(200).json(result);
   } catch (error) {
     console.log(error);
   }
 });
+
+//GET a specific post details
+router.get("/get-upload-details/:upload_id", async (req, res) => {
+  try {
+    const { upload_id } = req.params;
+    const query = await pool.query("SELECT * FROM uploads WHERE upload_id = $1", [upload_id]);
+    const result = query.rows[0];
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// //get the images from out list of images
+// router.get("/post-image", async (req, res) => {
+//   //get resources from recipegram folder
+//   const { resources } = await cloudinary.search
+//     .expression("folder: recipegram")
+//     .sort_by("public_id", "desc")
+//     .max_results(30)
+//     .execute();
+//   const publicIds = resources.map((file) => file.public_id);
+
+//   res.send(publicIds);
+// });
 
 module.exports = router;
