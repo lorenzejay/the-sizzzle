d092e757117feb35b62ea3ceb70e20078bd1d6c4
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-01 09:46:57 -0800

    redux series for getting uploads

--- client/src/redux/Actions/uploadActions.js
+++ client/src/redux/Actions/uploadActions.js
@@ -5,6 +5,12 @@ import {
   GET_ALL_USER_UPLOADS_REQUEST,
   GET_ALL_USER_UPLOADS_SUCCESS,
   GET_ALL_USER_UPLOADS_FAIL,
+  GET_LOGGED_IN_USER_FOLLOWING_POSTS_REQUEST,
+  GET_LOGGED_IN_USER_FOLLOWING_POSTS_SUCCESS,
+  GET_LOGGED_IN_USER_FOLLOWING_POSTS_FAIL,
+  GET_UPLOAD_DETAILS_REQUEST,
+  GET_UPLOAD_DETAILS_SUCCESS,
+  GET_UPLOAD_DETAILS_FAIL,
 } from "../Types/uploadTypes";
 
 export const uploadUsersPost = (title, description, base64EncodedImage) => async (
@@ -16,7 +22,7 @@ export const uploadUsersPost = (title, description, base64EncodedImage) => async
       userLogin: { userInfo },
     } = getState();
     dispatch({ type: UPLOAD_POST_REQUEST });
-    const data = await fetch("http://localhost:5000/upload", {
+    const data = await fetch("http://localhost:5000/api/upload", {
       method: "POST",
       headers: { token: `${userInfo.token}`, "Content-type": "application/json" },
       body: JSON.stringify({ data: base64EncodedImage, title, description }),
@@ -32,7 +38,7 @@ export const uploadUsersPost = (title, description, base64EncodedImage) => async
 export const getAllUserPosts = (user_id) => async (dispatch) => {
   try {
     dispatch({ type: GET_ALL_USER_UPLOADS_REQUEST });
-    const data = await fetch(`http://localhost:5000/upload/get-user-post/${user_id}`, {
+    const data = await fetch(`http://localhost:5000/api/upload/get-user-post/${user_id}`, {
       method: "GET",
       headers: { "Content-type": "application/json" },
     });
@@ -41,5 +47,39 @@ export const getAllUserPosts = (user_id) => async (dispatch) => {
   } catch (error) {
     console.log(error.message);
     dispatch({ type: GET_ALL_USER_UPLOADS_FAIL, payload: error.message });
+  }
+};
+
+export const getAllCurrentUserFollowingsPost = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: GET_LOGGED_IN_USER_FOLLOWING_POSTS_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const data = await fetch(`http://localhost:5000/api/upload/user-following-post`, {
+      method: "GET",
+      headers: { token: userInfo.token, "Content-type": "application/json" },
+    });
+    const parsedData = await data.json();
+    dispatch({ type: GET_LOGGED_IN_USER_FOLLOWING_POSTS_SUCCESS, payload: parsedData });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({ type: GET_LOGGED_IN_USER_FOLLOWING_POSTS_FAIL, payload: error.message });
+  }
+};
+
+export const getUploadDetails = (upload_id) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_UPLOAD_DETAILS_REQUEST });
+    const data = await fetch(`http://localhost:5000/api/upload/details/${upload_id}`, {
+      method: "GET",
+      headers: { "Content-type": "application/json" },
+    });
+    const parsedData = await data.json();
+    dispatch({ type: GET_UPLOAD_DETAILS_SUCCESS, payload: parsedData });
+  } catch (error) {
+    console.log(error.message);
+    dispatch({ type: GET_UPLOAD_DETAILS_FAIL, payload: error.message });
   }
 };
--- client/src/redux/Reducers/uploadReducer.js
+++ client/src/redux/Reducers/uploadReducer.js
@@ -5,6 +5,12 @@ import {
   GET_ALL_USER_UPLOADS_REQUEST,
   GET_ALL_USER_UPLOADS_SUCCESS,
   GET_ALL_USER_UPLOADS_FAIL,
+  GET_LOGGED_IN_USER_FOLLOWING_POSTS_REQUEST,
+  GET_LOGGED_IN_USER_FOLLOWING_POSTS_SUCCESS,
+  GET_LOGGED_IN_USER_FOLLOWING_POSTS_FAIL,
+  GET_UPLOAD_DETAILS_REQUEST,
+  GET_UPLOAD_DETAILS_SUCCESS,
+  GET_UPLOAD_DETAILS_FAIL,
 } from "../Types/uploadTypes";
 
 export const uploadPostReducer = (state = {}, action) => {
@@ -26,6 +32,32 @@ export const getAllUserUploadsReducer = (state = { allPosts: [] }, action) => {
     case GET_ALL_USER_UPLOADS_SUCCESS:
       return { loading: false, allPosts: action.payload };
     case GET_ALL_USER_UPLOADS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const getLoggedInUserFollowingPostsReducer = (state = { posts: [] }, action) => {
+  switch (action.type) {
+    case GET_LOGGED_IN_USER_FOLLOWING_POSTS_REQUEST:
+      return { loading: true };
+    case GET_LOGGED_IN_USER_FOLLOWING_POSTS_SUCCESS:
+      return { loading: false, posts: action.payload };
+    case GET_LOGGED_IN_USER_FOLLOWING_POSTS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const postDetailsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case GET_UPLOAD_DETAILS_REQUEST:
+      return { loading: true };
+    case GET_UPLOAD_DETAILS_SUCCESS:
+      return { loading: false, details: action.payload };
+    case GET_UPLOAD_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
