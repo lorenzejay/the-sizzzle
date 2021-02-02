84ba2a3b7ca4daed448243a1fed01bc0139519ee
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-02 05:13:08 -0800

    updated to remove the path to include dashboard to go to a users profile

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route exact path="/" component={Homepage} />
       <Route path="/sign-up" component={SignUp} />
       <Route path="/login" component={SignIn} />
-      <Route exact path="/dashboard/:id" component={Dashboard} />
+      <Route exact path="/:id" component={Dashboard} />
       <Route path="/dashboard/:id/edit-profile" component={EditProfilePage} />
       <Route path="/upload" component={Upload} />
       <Route path="/post/:id" component={PostTemplate} />
