765fbafa1cf8429d8eed0217c536d640f0ca7efb
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-05 12:23:06 -0800

    show random posts when there is a logged in user

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -12,6 +12,8 @@ const Homepage = () => {
   const userFollowingsPosts = useSelector((state) => state.userFollowingsPosts);
   const { loading, error, posts } = userFollowingsPosts;
 
+  const [randomPosts, setRandomPosts] = useState([]);
+
   //if you are logged in
   useEffect(() => {
     if (userInfo) {
@@ -19,7 +21,23 @@ const Homepage = () => {
     }
   }, [dispatch, userInfo]);
 
-  //get featured posts
+  //if there is no user logged in get random posts
+  const getRandomPosts = async () => {
+    const data = await fetch("http://localhost:5000/api/upload/random", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    //returns the array of the random posts
+    const parsedData = await data.json();
+    setRandomPosts(parsedData);
+  };
+  useEffect(() => {
+    if (!userInfo) {
+      getRandomPosts();
+    }
+  }, [userInfo]);
+
+  //set the time
   const diffTime = (dt1, dt2) => {
     var diff = (dt2.getTime() - dt1.getTime()) / 1000;
     diff /= 60 * 60;
@@ -38,6 +56,31 @@ const Homepage = () => {
       <div className="flex flex-col gap-10 pt-20 md:gap-20 lg:gap-24">
         {posts &&
           posts.map((post) => {
+            const dt1 = new Date(post.created_at);
+            const dt2 = new Date();
+
+            return (
+              <Link
+                to={{
+                  pathname: `/post/${post.upload_id}`,
+                  state: {
+                    uploadId: post.upload_id,
+                  },
+                }}
+                className="w-full md:w-1/2 mx-auto"
+                key={post.upload_id}
+              >
+                <img src={post.image_url} loading="lazy" className=" object-cover" />
+                <div className="p-5 lg:p-0 mt-3">
+                  <h3 className="text-xl">{post.title}</h3>
+                  <h3 className="text-md">{post.description}</h3>
+                  <p>{diffTime(dt1, dt2)} </p>
+                </div>
+              </Link>
+            );
+          })}
+        {randomPosts &&
+          randomPosts.map((post) => {
             const dt1 = new Date(post.created_at);
             const dt2 = new Date();
 
