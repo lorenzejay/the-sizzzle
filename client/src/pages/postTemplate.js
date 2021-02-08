ddb6e849189fc30e1b7774ed2521433d203ba50a
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-08 09:38:32 -0800

    style addrressed and updated

--- client/src/pages/postTemplate.js
+++ client/src/pages/postTemplate.js
@@ -3,16 +3,31 @@ import Layout from "../components/layout";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { AiOutlineHeart } from "react-icons/ai";
-import { BsBookmark } from "react-icons/bs";
 import { getUploadDetails } from "../redux/Actions/uploadActions";
 import UploaderProfileBar from "../components/uploaderProfileBar";
+import SavePostButton from "../components/savePostButton";
+import * as Showdown from "showdown";
+import dompurify from "dompurify";
+import { FaTrash } from "react-icons/fa";
+import PaddingWrapper from "../components/paddingWrapper";
 
-const PostTemplate = () => {
+const PostTemplate = ({ location }) => {
   const dispatch = useDispatch();
-  const [isUserLoginPost, setIsUserLoginPost] = useState(false);
+  const path = location.pathname;
+  const uploadIdFromPath = path.substring(6, path.length);
+  console.log(uploadIdFromPath);
 
-  const data = useLocation();
-  const { state } = data;
+  //converting markdown from description of upload to html
+  const sanitizer = dompurify.sanitize;
+  const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+  });
+
+  const [isUserLoginPost, setIsUserLoginPost] = useState(false);
+  const [mdToHtml, setMdToHtml] = useState();
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -20,42 +35,71 @@ const PostTemplate = () => {
   const uploadDetails = useSelector((state) => state.uploadDetails);
   const { details, loading, error } = uploadDetails;
 
+  // console.log(state);
+  // console.log(userInfo);
   //check if this is users post
   useEffect(() => {
-    if (userInfo && userInfo.returnedUserId === state.uploadedBy) {
+    if (userInfo && details && userInfo.returnedUserId === details.uploaded_by) {
       setIsUserLoginPost(true);
     }
-  }, [userLogin]);
+  }, [userLogin, details]);
 
   useEffect(() => {
-    dispatch(getUploadDetails(state.uploadId));
+    dispatch(getUploadDetails(uploadIdFromPath));
   }, [dispatch]);
 
   //   console.log(userInfo);
-  console.log(state);
-  //   console.log(state);
+  // console.log(details.description);
+  useEffect(() => {
+    if (details) {
+      var html = details && converter.makeHtml(details.description);
+      setMdToHtml(html);
+    }
+  }, [details]);
 
+  const convertDate = (date) => {
+    // return new Date(date).toString().slice(4, 15).replaceAt(6, ", ");
+    return new Date(date).toLocaleString().slice(0, 8);
+  };
+  details && console.log(isUserLoginPost);
   return (
     <Layout>
-      <div className="px-2 md:px-20 pt-10">
-        {!isUserLoginPost ? (
-          <span className="flex gap-3 justify-end">
-            <AiOutlineHeart size={24} />
-            <BsBookmark size={24} />
-          </span>
-        ) : (
-          <Link to="/" className="flex gap-3 justify-end">
-            Edit
-          </Link>
-        )}
-      </div>
       {details && (
-        <div className="md:px-20 py-5 flex flex-col justify-center w-1/2 mx-auto">
-          <UploaderProfileBar uploaded_by={state.uploaded_by} className="w-full " />
-          <img src={details.image_url} className="h-96 object-contain bg-gray-900" />
-          <h2 className="text-4xl my-5">{details.title}</h2>
-          <p className=" float-left w-full lg:w-1/2 px-5 md:px-0">{details.description}</p>
-        </div>
+        <>
+          <div className="px-5 lg:px-72 pt-10">
+            {!isUserLoginPost ? (
+              <span className="flex gap-3 justify-end">
+                <AiOutlineHeart size={24} />
+                <SavePostButton upload_id={details.uploadId} />
+              </span>
+            ) : (
+              <div className="flex items-center gap-3 justify-end">
+                <Link to="/" className="px-5 py-0.5 bg-gray-600 rounded-md text-white ">
+                  Edit
+                </Link>
+                <FaTrash className="cursor-pointer" />
+              </div>
+            )}
+          </div>
+          {details && (
+            <PaddingWrapper>
+              <h2 className="text-5xl font-bold my-5">{details.title}</h2>
+              <div className="flex items-center gap-5">
+                <UploaderProfileBar uploaded_by={details.uploaded_by} className="w-full " />
+                <p className="my-4 pb-3 text-gray-400">{convertDate(details.created_at)}</p>
+              </div>
+              <img src={details.image_url} className="relative object-cover " />
+
+              <div className=" md:px-0">
+                <p className="mb-5">{details.caption}</p>
+                <div
+                  dangerouslySetInnerHTML={{ __html: sanitizer(mdToHtml) }}
+                  className="post-description text-xl"
+                ></div>
+              </div>
+            </PaddingWrapper>
+          )}
+        </>
       )}
     </Layout>
   );
