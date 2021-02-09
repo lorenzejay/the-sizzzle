566088d11a75b18dae4c4bb8cf81d1cbce3adb92
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-09 07:08:13 -0800

    created a padding wrapper for reuseable components

--- client/src/components/paddingWrapper.js
+++ client/src/components/paddingWrapper.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const PaddingWrapper = ({ children }) => {
   return (
-    <div className="w-full mx-auto px-5 pt-5 pb-10 sm:px-24 md:px-48 lg:px-72">{children}</div>
+    <div className="padding-wrapper w-full mx-auto px-5 pt-5 pb-10 sm:px-24 md:px-48 lg:px-72">
+      {children}
+    </div>
   );
 };
 
