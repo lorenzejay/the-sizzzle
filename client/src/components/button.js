dca11b6556c5e034c9a1825f4e3bde02f66ba5a8
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-09 07:07:12 -0800

    vertical padding reduced

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, type, className }) => {
   return (
     <button
-      className={`bg-gray-900 text-white text-xl py-3 px-4 mx-auto rounded-md ${className}`}
+      className={`bg-gray-900 text-white text-xl py-1 px-4 mx-auto rounded-md ${className}`}
       type={type}
     >
       {children}
