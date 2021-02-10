cad5fe60f663b0a5403f35ebff4f8b48f5bb66cc
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-10 05:37:45 -0800

    added follow action from state to trigger a getFollowers dispatch and removed the loader when following for ui purposes

--- client/src/components/followersTab.js
+++ client/src/components/followersTab.js
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getFollowers } from "../redux/Actions/followersAction";
 import ErrorMessage from "./errorMessage";
-import Loader from "./loader";
 
-const FollowersTab = ({ anyUserProfile, follow }) => {
+const FollowersTab = ({ anyUserProfile }) => {
   const dispatch = useDispatch();
   const getFollows = useSelector((state) => state.getFollows);
-  const { loading, followers, error } = getFollows;
+  const { followers, error } = getFollows;
+  //follow a user action.
+  const followUser = useSelector((state) => state.follow);
+  const { follow } = followUser;
 
   useEffect(() => {
     if (anyUserProfile && anyUserProfile.user) {
@@ -17,7 +19,6 @@ const FollowersTab = ({ anyUserProfile, follow }) => {
 
   return (
     <div className="followers flex flex-row gap-5">
-      {loading && <Loader />}
       {error && <ErrorMessage>{error}</ErrorMessage>}
       {followers && (
         <div className="text-xl flex gap-10 mt-5">
