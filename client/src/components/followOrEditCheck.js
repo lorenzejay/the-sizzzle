64b58a749cf05d5cbfee5aeb8f0cdc7feb28b748
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-10 04:51:35 -0800

    removed checks with userInfo and use loggedInUserDetails instead

--- client/src/components/followOrEditCheck.js
+++ client/src/components/followOrEditCheck.js
@@ -3,12 +3,17 @@ import { FiUserCheck } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { checkIfUserIsFollowingAlready, followThisUser } from "../redux/Actions/followersAction";
+import Loader from "./loader";
 
-const FollowOrEditCheck = ({ isLoggedInUserProfile, userInfo, anyUserProfile }) => {
+const FollowOrEditCheck = ({ isLoggedInUserProfile, anyUserProfile }) => {
   const dispatch = useDispatch();
 
   const checkIfFollowing = useSelector((state) => state.checkIfFollowing);
   const { isFollowing } = checkIfFollowing;
+
+  //if there is a logged in user
+  const userLoggedInDetails = useSelector((state) => state.userLoggedInDetails);
+  const { loggedInUserDetails } = userLoggedInDetails;
 
   //follow a user action.
   const followUser = useSelector((state) => state.follow);
@@ -16,47 +21,52 @@ const FollowOrEditCheck = ({ isLoggedInUserProfile, userInfo, anyUserProfile }) 
 
   //check if the user is following
   useEffect(() => {
-    if (anyUserProfile && userInfo && anyUserProfile !== userInfo.returnedUserId) {
+    if (anyUserProfile && loggedInUserDetails && anyUserProfile !== loggedInUserDetails.user_id) {
       dispatch(checkIfUserIsFollowingAlready(anyUserProfile.user.user_id));
     }
-  }, [dispatch, anyUserProfile, follow]);
+  }, [dispatch, anyUserProfile, follow, loggedInUserDetails]);
 
   const followCommand = () => {
     //cannot follow yourself
-    if (anyUserProfile.user.user_id !== userInfo.returnedUserId) {
+    if (anyUserProfile.user.user_id !== loggedInUserDetails.user_id) {
       dispatch(followThisUser(anyUserProfile.user.user_id));
     }
   };
 
   return (
-    <>
-      {isLoggedInUserProfile && userInfo ? (
-        <Link
-          className="bg-gray-500 px-10 rounded text-white py-1 my-5"
-          to={{
-            pathname: `/dashboard/${anyUserProfile.user.username}/edit-profile`,
-            state: {
-              user: userInfo.returnedUserId,
-            },
-          }}
-        >
-          Edit Profile
-        </Link>
-      ) : userInfo && userInfo.returnedUsername ? (
-        <button
-          className={`px-10 rounded text-white py-1 my-5 focus:outline-none w-40 h-8 text-center transition duration-500 ease-in-out block ${
-            isFollowing ? "bg-gray-300" : "bg-red-500"
-          }`}
-          onClick={followCommand}
-        >
-          {isFollowing ? <FiUserCheck className="mx-auto" size={16} /> : "Follow"}
-        </button>
-      ) : (
-        <Link to="/login" className="bg-red-500 px-10 rounded text-white py-1 my-5 block">
-          Login to follow
-        </Link>
+    <div className="my-10">
+      {anyUserProfile && (
+        <>
+          {loadFollow && <Loader />}
+          {isLoggedInUserProfile && loggedInUserDetails ? (
+            <Link
+              className="bg-gray-500 px-10 rounded text-white py-1 my-5"
+              to={{
+                pathname: `/dashboard/${loggedInUserDetails.username}/edit-profile`,
+                state: {
+                  user: loggedInUserDetails.user_id,
+                },
+              }}
+            >
+              Edit Profile
+            </Link>
+          ) : loggedInUserDetails ? (
+            <button
+              className={`px-10 rounded text-white py-1 my-5 focus:outline-none w-40 h-8 text-center transition duration-500 ease-in-out block ${
+                isFollowing ? "bg-gray-300" : "bg-red-500"
+              }`}
+              onClick={followCommand}
+            >
+              {isFollowing ? <FiUserCheck className="mx-auto" size={16} /> : "Follow"}
+            </button>
+          ) : (
+            <Link to="/login" className="bg-red-500 px-10 rounded text-white py-1 my-5 block">
+              Login to follow
+            </Link>
+          )}
+        </>
       )}
-    </>
+    </div>
   );
 };
 
