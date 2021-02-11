033de909ba33b6f1b621e496f996ffc653a5a3e8
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-10 18:47:02 -0800

    refactored dashboard

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import { useSelector, useDispatch } from "react-redux";
-import DefaultPP from "../images/dpp.png";
 import styled from "styled-components";
 import { getAnyUserDetails } from "../redux/Actions/userActions";
 import Loader from "../components/loader";
-import { Link } from "react-router-dom";
-import { getAllUserPosts } from "../redux/Actions/uploadActions";
 import ErrorMessage from "../components/errorMessage";
 import FollowersTab from "../components/followersTab";
 import FollowOrEditCheck from "../components/followOrEditCheck";
+import LoggedInUserProfilePicture from "../components/loggedInUserProfilePicture";
+import DashboardPosts from "../components/dashboardPosts";
 
 export const PostThumnails = styled.div``;
 
@@ -29,10 +28,6 @@ const Dashboard = ({ location }) => {
   const userLoggedInDetails = useSelector((state) => state.userLoggedInDetails);
   const { loggedInUserDetails } = userLoggedInDetails;
 
-  //get the users posts
-  const allUserPosts = useSelector((state) => state.allUserPosts);
-  const { allPosts, loading: loadPosts, error: errorPosts } = allUserPosts;
-
   //get the user info
   useEffect(() => {
     dispatch(getAnyUserDetails(queriedUser));
@@ -48,16 +43,10 @@ const Dashboard = ({ location }) => {
     }
   }, [queriedUser, loggedInUserDetails, anyUserProfile]);
 
-  //get users posts
-  useEffect(() => {
-    if (anyUserProfile && anyUserProfile.user) {
-      dispatch(getAllUserPosts(anyUserProfile.user.user_id));
-    }
-  }, [dispatch, anyUserProfile]);
-
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+  //if user ever gets past 999 followers
+  // function numberWithCommas(x) {
+  //   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // }
 
   return (
     <Layout>
@@ -67,46 +56,20 @@ const Dashboard = ({ location }) => {
       {anyUserProfile && anyUserProfile.user && (
         <div className="px-10 flex flex-col">
           <div className="flex flex-col justify-center items-center mt-10">
-            <img
-              src={anyUserProfile.user.profilepic || DefaultPP}
-              alt="user profile thumnail"
-              title="profile picture"
-              className="rounded-full w-32 h-32 md:h-52 md:w-52 object-cover mb-10"
-            />
+            <LoggedInUserProfilePicture />
             <div className="flex flex-row font-bold text-3xl gap-3">
-              {/* <h2 className="lg:border-r-2">{`${anyUserProfile.user.first_name}  ${anyUserProfile.user.last_name}`}</h2>{" "} */}
               <h2>
-                {" "}
                 <span className="text-xl">@</span>
                 {anyUserProfile.user.username}
               </h2>
             </div>
             <FollowersTab anyUserProfile={anyUserProfile} />
-
             <FollowOrEditCheck
               isLoggedInUserProfile={isLoggedInUserProfile}
               anyUserProfile={anyUserProfile}
             />
           </div>
-
-          <div className="post-grid grid grid-cols-3 gap-5 justify-center mt-10 md:mx-auto md:gap-32">
-            {loadPosts && <Loader />}
-            {errorPosts && <h1 className="text-red-600">{errorPosts}</h1>}
-            {allPosts &&
-              allPosts.map((post) => (
-                <Link to={`/post/${post.upload_id}`} key={post.upload_id}>
-                  <img
-                    src={post.image_url}
-                    className="w-32 h-32 md:w-44 md:h-44 object-cover"
-                    loading="lazy"
-                    alt="post"
-                  />
-                </Link>
-              ))}
-            {allPosts && allPosts.length === 0 && (
-              <h2 className="flex items-center">No Posts Yet </h2>
-            )}
-          </div>
+          <DashboardPosts />
         </div>
       )}
     </Layout>
