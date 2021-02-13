c25bb26c912b54a788fa39c2898ae17c3e9a11a0
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-13 10:37:02 -0800

    configured for production

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
-const pool = new Pool({
-  user: "postgres",
-  password: "Subarachnoidspace123",
-  host: "localhost",
-  port: 5432,
-  database: "recipegram",
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL, //THIS COMES FROM HEROKU ADD ON
+};
+
+const pool = new Pool(process.env.NODE_ENV === "Production" ? prodConfig : devConfig);
 
 module.exports = pool;
