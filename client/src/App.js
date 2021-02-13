4fbbbe22589589916915c330f113b3a1c4b010ef
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-12 21:24:43 -0800

    added a saved posts route

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import EditProfilePage from "./pages/editProfilePage";
 import Upload from "./pages/upload";
 import PostTemplate from "./pages/postTemplate";
 import EditUpload from "./pages/editUpload";
+import SavedPosts from "./pages/savedPosts";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Route path="/upload" component={Upload} />
       <Route path="/post/:id" component={PostTemplate} />
       <Route exact path="/edit-post/:id" component={EditUpload} />
+      <Route exact path="/saved" component={SavedPosts} />
     </Router>
   );
 }
