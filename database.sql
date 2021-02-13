9076b9f93b7b2145ca13705f97bfec79fe368d1f
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-13 13:21:54 -0800

    database.sql file updated to current

--- database.sql
+++ database.sql
@@ -1,15 +1,16 @@
 CREATE DATABASE RecipeGram;
 
 --download extension uuid--
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- needs to change varchar to 50 --
 CREATE TABLE users(
     user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(55) NOT NULL,
+    first_name VARCHAR(55) NOT NULL,
+    last_name VARCHAR(55) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    profilePic VARCHAR(300) DEFAULT ''
+    profilepic VARCHAR(300) DEFAULT ''
 );
 
 ALTER TABLE users
@@ -21,16 +22,20 @@ INSERT INTO users(username, first_name, last_name, email, password) VALUES ('Joh
 
 CREATE TABLE followers(
     followers_id SERIAL PRIMARY KEY,
-    user_to uuid REFERENCES users(user_id) NOT NULL,
-    user_from uuid REFERENCES users(user_id) NOT NULL,
+    user_to uuid  NOT NULL,
+    user_from uuid NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+-- alter table followers drop constraint followers_user_from_fkey;
+-- alter table followers drop constraint followers_user_to_fkey;
+alter table followers add foreign key(user_to) references users(user_id) on delete cascade;
+alter table followers add foreign key(user_from) references users(user_id) on delete cascade;
 
 -- INSERT INTO followers(user_to, user_from) VALUES ('7e253413-c371-4a3c-84bf-12ef45861795', '8e96a151-7882-4002-abb1-f1b3b411c754');
 
 CREATE TABLE uploads(
     upload_id SERIAL PRIMARY KEY,
-    uploaded_by uuid REFERENCES users(user_id) NOT NULL,
+    uploaded_by uuid NOT NULL,
     title VARCHAR(50) NOT NULL,
     caption TEXT NOT NULL,
     description TEXT NOT NULL,
@@ -38,6 +43,9 @@ CREATE TABLE uploads(
     image_url VARCHAR(128) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+alter table uploads add foreign key(uploaded_by) references users(user_id) on delete cascade;
+-- alter table followers drop constraint followers_user_to_fkey;
 
 CREATE TABLE saved_uploads(
     saved_upload_id SERIAL PRIMARY KEY,
