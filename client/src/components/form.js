06734f84c5a22fa35d03c93976d48863f2b26112
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-12 21:26:10 -0800

    added styling

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Form = ({ title, children, handleSubmit }) => {
+const Form = ({ title, children, handleSubmit, className }) => {
   return (
-    <form className="border-1 p-5 flex flex-col" onSubmit={(e) => handleSubmit(e)}>
-      <h1 className="text-center text-5xl font-bold mb-6">{title}</h1>
+    <form
+      className={`border-1 p-5 flex flex-col w-full ${className}`}
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      <h1 className="text-center text-5xl font-bold mb-6 ">{title}</h1>
       {children}
     </form>
   );
