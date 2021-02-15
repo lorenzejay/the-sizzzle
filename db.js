185d4a968930562bbb913919891550b825d542fc
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-14 20:26:11 -0800

    added config

--- db.js
+++ db.js
@@ -11,6 +11,9 @@ const devConfig = {
 
 const prodConfig = {
   connectionString: process.env.DATABASE_URL, //THIS COMES FROM HEROKU ADD ON
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const pool = new Pool(process.env.NODE_ENV === "Production" ? prodConfig : devConfig);
