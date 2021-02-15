918f0cba0c4bf368fe9ff2fcd63de87e25f1595b
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-14 17:14:52 -0800

    created a direction list component

---
+++ client/src/components/directionsPreviews.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DirectionPreviews = ({ directionList }) => {
+  return (
+    <>
+      <h3 className="font-bold text-3xl">Directions</h3>
+      <ol className="list-decimal">
+        {directionList &&
+          directionList.map((item, i) => {
+            return (
+              <li className="ml-4 my-1 text-lg " key={i}>
+                {item}
+              </li>
+            );
+          })}
+      </ol>
+    </>
+  );
+};
+
+export default DirectionPreviews;
