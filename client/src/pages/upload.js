7568a44f9e8de206cba9be2d5f05548ec8f4cbd5
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-14 17:02:59 -0800

    added previews and updated inputs

--- client/src/pages/upload.js
+++ client/src/pages/upload.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout";
 import Loader from "../components/loader";
 // import RichTextEditor from "../components/richTextEditor";
 import { uploadUsersPost } from "../redux/Actions/uploadActions";
-import RichTextEditor from "../components/richTextEditor";
+import ErrorMessage from "../components/errorMessage";
 
 const Upload = ({ history }) => {
   const dispatch = useDispatch();
@@ -20,8 +20,11 @@ const Upload = ({ history }) => {
   const [previewSource, setPreviewSource] = useState("");
 
   const [title, setTitle] = useState("");
-  const [caption, setCaption] = useState("");
-  const [description, setDescription] = useState("");
+  const [ingredient, setIngredient] = useState("");
+  const [ingredientList, setIngredientList] = useState([]);
+  const [direction, setDirection] = useState("");
+  const [directionList, setDirectionList] = useState([]);
+  const [uploadError, setUploadError] = useState([]);
 
   useEffect(() => {
     if (!userInfo) {
@@ -30,7 +33,7 @@ const Upload = ({ history }) => {
   }, [dispatch, history, userInfo]);
 
   useEffect(() => {
-    if (postResult && !error) {
+    if (postResult && postResult.success) {
       history.push("/");
     }
   }, [dispatch, history, postResult]);
@@ -50,60 +53,159 @@ const Upload = ({ history }) => {
   };
   const handleSubmitPost = (e) => {
     e.preventDefault();
-    if (!previewSource || !title || !description || !caption) return;
+    if (!previewSource || !title || ingredientList.length === 0) {
+      setUploadError("Nothing must be blank before uploading");
+    }
     // uploadImage(previewSource);
-    dispatch(uploadUsersPost(title, caption, description, previewSource));
+    dispatch(uploadUsersPost(title, ingredientList, directionList, previewSource));
     if (postResult && !error) {
       history.push("/");
     }
   };
+  const handleIngredients = () => {
+    if (ingredient !== "") {
+      setIngredientList([...ingredientList, ingredient]);
+      setIngredient("");
+    } else {
+      window.alert("Invisble ingredient? I think not. Please input an ingredient.");
+    }
+  };
 
+  const undoLastIngredient = () => {
+    const lastItem = ingredientList.pop();
+    // console.log(lastItem);
+    const filteredList = ingredientList.filter((list) => list !== lastItem);
+    setIngredientList(filteredList);
+  };
+
+  const handleDirectionLists = () => {
+    if (direction !== "") {
+      setDirectionList([...directionList, direction]);
+      setDirection("");
+    } else {
+      window.alert("Invisble direction? I think not. Please input an direction.");
+    }
+  };
+
+  const undoLastDirection = () => {
+    const lastItem = directionList.pop();
+    // console.log(lastItem);
+    const filteredList = directionList.filter((list) => list !== lastItem);
+    setDirectionList(filteredList);
+  };
+
+  // console.log(title);
   return (
     <Layout>
       <div>
         {loading && <Loader />}
-        <form
-          onSubmit={handleSubmitPost}
-          className="flex flex-col justify-center items-center padding"
-        >
+        <form onSubmit={handleSubmitPost} className="flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold uppercase my-10">
             Post your fire dishes <span>🔥</span>
           </h1>
+          {uploadError && (
+            <div className={`${uploadError && "hidden"}`}>
+              <ErrorMessage>{uploadError}</ErrorMessage>
+            </div>
+          )}
+          {previewSource && (
+            <div className="preview flex flex-col w-3/4 md:w-1/2">
+              <h3 className="">Preview</h3>
+              <h2 className="text-5xl font-bold my-5">{title}</h2>
+              {previewSource && (
+                <img
+                  src={previewSource}
+                  alt="Preview of upload media."
+                  className="relative object-cover max-h-screen w-full"
+                />
+              )}
+              <h3 className="font-bold text-3xl">Ingredients</h3>
+              <ul>
+                {ingredientList &&
+                  ingredientList.map((item, i) => {
+                    return (
+                      <li className="flex flex-row gap-3 items-center my-1 text-lg" key={i}>
+                        <input type="checkbox" />
+                        {item}
+                      </li>
+                    );
+                  })}
+              </ul>
+              <h3 className="font-bold text-3xl">Directions</h3>
+              <ol className="list-decimal">
+                {directionList &&
+                  directionList.map((item, i) => {
+                    return (
+                      <li className="ml-4 my-1 text-lg " key={i}>
+                        {item}
+                      </li>
+                    );
+                  })}
+              </ol>
+            </div>
+          )}
           <Input type="file" name="image" onChange={handleFileInputState} value={fileInputState} />
           <Input
             type="text"
             name="title"
             placeholder="A non-boring title here."
-            onChange={(e) => setTitle(e.target.value)}
             value={title}
-          />
-          <textarea
-            name="caption"
-            onChange={(e) => setCaption(e.target.value)}
-            value={caption}
-            rows="3"
-            placeholder="Insert your fire caption here."
-            className="px-5 py-2 rounded w-3/4 md:w-1/2 mx-auto my-3 "
+            onChange={(e) => setTitle(e.target.value)}
           />
 
-          <RichTextEditor description={description} setDescription={setDescription} />
-
-          <Button type="submit">Submit</Button>
-        </form>
-
-        <div className="preview flex flex-col justify-center mx-auto">
-          {previewSource && (
-            <img
-              src={previewSource}
-              alt="Preview of upload media."
-              style={{ height: "300px" }}
-              className="mx-auto mt-10"
+          <div className="flex flex-row w-3/4 md:w-1/2 my-3 items-center">
+            <Input
+              type="text"
+              name="ingredients"
+              placeholder="List your secret ingredients"
+              onChange={(e) => setIngredient(e.target.value)}
+              value={ingredient}
+              className="flex-grow"
             />
-          )}
-          {/* {title && <h3 className="text-lg">{title}</h3>}
-          {caption && <p className="text-lg">{caption}</p>}
-          {description && <p>{description}</p>} */}
-        </div>
+            <button
+              type="button"
+              onClick={handleIngredients}
+              className="w-auto cursor-pointer outline-none hover:bg-green-400 text-xl px-2"
+            >
+              +
+            </button>
+            <button
+              type="button"
+              onClick={undoLastIngredient}
+              className="w-auto cursor-pointer outline-none hover:bg-red-400 text-xl px-2"
+            >
+              Undo
+            </button>
+          </div>
+
+          <div className="flex flex-row w-3/4 md:w-1/2 my-3 items-center">
+            <textarea
+              name="direction"
+              placeholder="Directions"
+              onChange={(e) => setDirection(e.target.value)}
+              value={direction}
+              className="flex-grow px-5 py-2 rounded"
+            />
+            <button
+              type="button"
+              onClick={handleDirectionLists}
+              className="w-auto cursor-pointer outline-none hover:bg-green-400 text-xl h-auto px-2"
+            >
+              +
+            </button>
+            <button
+              type="button"
+              onClick={undoLastDirection}
+              className="w-auto cursor-pointer outline-none hover:bg-red-400 text-xl h-auto px-2"
+            >
+              Undo
+            </button>
+          </div>
+          {/* <RichTextEditor description={description} setDescription={setDescription} /> */}
+          <Button type="submit" className="w-3/4 md:w-1/2">
+            Post
+          </Button>
+        </form>
       </div>
     </Layout>
   );
