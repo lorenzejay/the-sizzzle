06ffd1f284618344cf2789d8cb6a4fe8a3fbe826
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-14 18:06:21 -0800

    removed unused

--- client/src/components/profileOptionsDropdown.js
+++ client/src/components/profileOptionsDropdown.js
@@ -18,9 +18,8 @@ const ProfileOptionsDropdown = ({ profileDropdown, location }) => {
     dispatch(logout());
   };
 
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
+  // console.log(loggedInUserDetails.username);
+
   return (
     <div
       className={` shadow-lg absolute top-20 right-0 lg:right-24 min-h-48 w-72 transition-all duration-500 z-10 bg-white  ${
@@ -49,36 +48,43 @@ const ProfileOptionsDropdown = ({ profileDropdown, location }) => {
               </Link>
             </div>
           </section>
-          <ul className="flex flex-col justify-start items-start gap-3 p-5 text-md font-medium text-gray-500   transition-all duration-500">
-            <li>
-              <Link to="/upload" className="hover:text-black">
-                Write a dish
-              </Link>
-            </li>
-            <li>
-              <Link to={`/dashboard/${loggedInUserDetails.username}`} className="hover:text-black">
-                Profile
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={`/dashboard/${loggedInUserDetails.username}/edit-profile`}
-                className="hover:text-black"
-              >
-                Settings
-              </Link>
-            </li>
-            <li>
-              <Link to="/saved" className="hover:text-black">
-                Saved
-              </Link>
-            </li>
-            <li>
-              <button to="/saved" className="hover:text-black" onClick={handleLogout}>
-                Sign Out
-              </button>
-            </li>
-          </ul>
+          {loggedInUserDetails && (
+            <ul className="flex flex-col justify-start items-start gap-3 p-5 text-md font-medium text-gray-500   transition-all duration-500">
+              <li>
+                <Link to="/upload" className="hover:text-black">
+                  Write a dish
+                </Link>
+              </li>
+              <li>
+                {loggedInUserDetails.username && (
+                  <Link
+                    to={`/dashboard/${loggedInUserDetails.username}`}
+                    className="hover:text-black"
+                  >
+                    Profile
+                  </Link>
+                )}
+              </li>
+              <li>
+                <Link
+                  to={`/dashboard/${loggedInUserDetails.username}/edit-profile`}
+                  className="hover:text-black"
+                >
+                  Settings
+                </Link>
+              </li>
+              <li>
+                <Link to="/saved" className="hover:text-black">
+                  Saved
+                </Link>
+              </li>
+              <li>
+                <button to="/saved" className="hover:text-black" onClick={handleLogout}>
+                  Sign Out
+                </button>
+              </li>
+            </ul>
+          )}
         </div>
       )}
     </div>
