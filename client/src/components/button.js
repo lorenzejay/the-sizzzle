9708e0b27ffe69ff0defeead9106960c61204b5f
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-18 07:37:17 -0800

    updated styling and disabled props

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Button = ({ children, type, className }) => {
+const Button = ({ children, type, disabled, className, onClick }) => {
   return (
     <button
-      className={`bg-gray-900 text-white text-xl py-1 px-4 mx-auto rounded-md ${className}`}
+      className={`bg-gray-900 text-white text-xl py-1 px-4 mx-auto rounded-md disabled:opacity-50 ${className}`}
       type={type}
+      disabled={disabled}
+      onClick={onClick}
     >
       {children}
     </button>
