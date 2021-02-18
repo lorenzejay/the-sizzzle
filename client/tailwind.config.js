e551043c13a90bd9f13ff9d686aeff2a4071f324
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-18 07:34:52 -0800

    added max heights

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    maxHeight: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+      limit: "400px",
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ["disabled"],
+    },
   },
   plugins: [],
 };
