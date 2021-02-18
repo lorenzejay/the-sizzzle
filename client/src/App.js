cbde04fc955d3a2551e1bd262c68fa909543418c
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-18 07:35:59 -0800

    added 4 upload routes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,9 @@ import Upload from "./pages/upload";
 import PostTemplate from "./pages/postTemplate";
 import EditUpload from "./pages/editUpload";
 import SavedPosts from "./pages/savedPosts";
+import UploadIngredientsAndDirections from "./pages/uploadIngredientsAndDirections";
+import UploadDifficulty from "./components/uploadDifficultyAndCategory";
+import UploadPreview from "./components/uploadPreview";
 
 function App() {
   return (
@@ -20,7 +23,15 @@ function App() {
       <Route path="/login" component={SignIn} />
       <Route path="/dashboard/:id/edit-profile" component={EditProfilePage} />
       <Route exact path="/dashboard/:id" component={Dashboard} />
-      <Route path="/upload" component={Upload} />
+      <Route
+        exact
+        path="/upload-ingredients-directions"
+        component={UploadIngredientsAndDirections}
+      />
+      <Route exact path="/upload-difficulty-category" component={UploadDifficulty} />
+      <Route exact path="/upload-preview" component={UploadPreview} />
+      <Route exact path="/upload" component={Upload} />
+
       <Route path="/post/:id" component={PostTemplate} />
       <Route exact path="/edit-post/:id" component={EditUpload} />
       <Route exact path="/saved" component={SavedPosts} />
