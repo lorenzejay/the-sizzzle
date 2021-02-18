dc936211c8b91eeaff9c60ee4218dd77f4b2b315
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-18 07:41:15 -0800

    serves as an upload timeline

---
+++ client/src/components/uploadProgressSlider.js
@@ -0,0 +1,132 @@
+import React from "react";
+import styled from "styled-components";
+
+export const ProgressTrack = styled.div`
+  position: absolute;
+  top: 5px;
+  width: 100%;
+  height: 5px;
+  background-color: #dfe3e4;
+  z-index: -1;
+`;
+export const ProgressStep = styled.button`
+  position: relative;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  cursor: default;
+  // Hide the final step's progress bar
+  &:last-child:after {
+    display: none;
+  }
+  &:before {
+    content: "✔";
+    display: flex;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    width: 10px;
+    height: 10px;
+    background: #fff;
+    border: 4px solid #dfe3e4;
+    border-radius: 100%;
+    color: #fff;
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    top: 6px;
+    left: 50%;
+    width: 0%;
+    transition: width 1s ease-in;
+    height: 5px;
+    background: #dfe3e2;
+    z-index: -1;
+  }
+  &.is-active {
+    color: blue;
+
+    &:before {
+      border: 4px solid #777;
+      animation: pulse 2s infinite;
+    }
+  }
+  &.is-complete {
+    color: #009900;
+
+    // Step's circle in complete state
+    &:before {
+      font-family: Font Awesome;
+      font-size: 20px;
+      color: #fff;
+      background: green;
+      border: 4px solid transparent;
+    }
+
+    // Step's progress bar in complete state
+    &:after {
+      background: #2183dd;
+      animation: nextStep 1s;
+      animation-fill-mode: forwards;
+    }
+  }
+  // Pulse animation for Step's circle in active state
+  @keyframes pulse {
+    0% {
+      box-shadow: 0 0 0 0 rgba(33, 131, 221, 0.4);
+    }
+    70% {
+      box-shadow: 0 0 0 10px rgba(33, 131, 221, 0);
+    }
+    100% {
+      box-shadow: 0 0 0 0 rgba(33, 131, 221, 0);
+    }
+  }
+  @keyframes nextStep {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+`;
+
+const UploadProgressSlider = ({ step1, step2, step3, step4, complete }) => {
+  //   console.log(step1, step2, step3, step4);
+  return (
+    <div className="relative flex w-full mb-10">
+      <ProgressTrack />
+      <ProgressStep id="step1" className={`progress-step ${step1 ? "is-active" : "is-complete"}`}>
+        Image and Title
+      </ProgressStep>
+
+      <ProgressStep
+        id="step2"
+        className={`progress-step ${step2 ? "is-active" : step3 || step4 ? "is-complete" : ""}`}
+      >
+        Recipe
+      </ProgressStep>
+
+      <ProgressStep
+        id="step3"
+        className={`progress-step ${step3 ? "is-active" : step4 ? "is-complete" : ""}`}
+      >
+        Tags
+      </ProgressStep>
+
+      <ProgressStep id="step4" className={`progress-step ${step4 ? "is-active" : ""}`}>
+        Preview
+      </ProgressStep>
+    </div>
+  );
+};
+
+UploadProgressSlider.defaultProps = {
+  step1: true,
+  step2: false,
+  step3: false,
+  step4: false,
+  complete: false,
+};
+
+export default UploadProgressSlider;
