a7556cacfd8bb3948fa03ff396dafb5e92b58c85
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-20 10:39:16 -0800

    removed mx-auto

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, type, disabled, className, onClick }) => {
   return (
     <button
-      className={`bg-gray-900 text-white text-xl py-1 px-4 mx-auto rounded-md disabled:opacity-50 ${className}`}
+      className={`bg-gray-900 text-white text-xl py-1 px-4 rounded-md disabled:opacity-50 ${className}`}
       type={type}
       disabled={disabled}
       onClick={onClick}
