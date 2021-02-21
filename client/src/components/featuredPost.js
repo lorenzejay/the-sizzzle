d434cca0d795d7e9ee8cc20d672178ccdf8566b8
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-21 10:14:36 -0800

    created a featured post component

---
+++ client/src/components/featuredPost.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import UploaderProfileBar from "./uploaderProfileBar";
+
+const FeaturedPost = () => {
+  //if there is a logged in user
+  const userLoggedInDetails = useSelector((state) => state.userLoggedInDetails);
+  const { loggedInUserDetails } = userLoggedInDetails;
+
+  const [featuredPosts, setFeaturedPosts] = useState([]);
+  const getFeaturedPosts = async () => {
+    try {
+      const data = await fetch("/api/upload/featured-posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const result = await data.json();
+      console.log(result);
+      setFeaturedPosts(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getFeaturedPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="font-medium mb-3">Trending on The Sizzzle</h2>
+      <section>
+        {loggedInUserDetails && (
+          <ul className="lg:flex gap-7">
+            {featuredPosts &&
+              featuredPosts.map((post, i) => {
+                const month = new Date(post.created_at).getMonth();
+                const date = new Date(post.created_at).getDate();
+                const year = new Date(post.created_at).getFullYear();
+
+                //bg color depending on level
+
+                return (
+                  <li className="mb-8">
+                    <Link to={`/post/${post.upload_id}`} className="featured-list-item flex ">
+                      <div className="flex">
+                        <span className="text-gray-400 text-3xl mr-3">0{i + 1}</span>
+                      </div>
+                      <div className="ml-3 pt-1">
+                        <UploaderProfileBar uploaded_by={post.uploaded_by} />
+                        <div>
+                          <h3 className="font-bold">{post.title}</h3>
+                          <div>
+                            {month}/{date}/{year} ·{" "}
+                            <span
+                              className={`${post.difficulty === "easy" && "bg-green-300"} ${
+                                post.difficulty === "medium" && "bg-yellow-300"
+                              } ${post.difficulty === "hard" && "bg-red-300"} ${
+                                post.difficulty === "extreme" && "bg-red-600"
+                              } px-3 `}
+                            >
+                              {post.difficulty.toUpperCase()}
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
+          </ul>
+        )}
+      </section>
+    </div>
+  );
+};
+
+export default FeaturedPost;
