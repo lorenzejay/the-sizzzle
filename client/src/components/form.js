b14b3cb68985067007b0be0f137eecac9c7a2a08
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-20 19:58:49 -0800

    removed unused classes

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -2,10 +2,7 @@ import React from "react";
 
 const Form = ({ title, children, handleSubmit, className }) => {
   return (
-    <form
-      className={`border-1 p-5 flex flex-col w-full ${className}`}
-      onSubmit={(e) => handleSubmit(e)}
-    >
+    <form className={`flex flex-col w-full ${className}`} onSubmit={(e) => handleSubmit(e)}>
       <h1 className="text-center text-5xl font-bold mb-6 ">{title}</h1>
       {children}
     </form>
